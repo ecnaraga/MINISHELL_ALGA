b602a5ec554483fc963e753f386d251f6268a3f0
gaga <garance.lambey@gmail.com>
2023-11-16 15:56:30 +0100

    gaga : re mise en place de parse bis on n efface plus

--- srcs/ft_parse_ter.c
+++ srcs/ft_parse_ter.c
@@ -6,7 +6,7 @@
 /*   By: galambey <galambey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 16:16:50 by galambey          #+#    #+#             */
-/*   Updated: 2023/11/16 15:02:12 by galambey         ###   ########.fr       */
+/*   Updated: 2023/11/16 15:26:05 by galambey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,39 @@ char	*ft_error_message_chevron(char *str, int skip)
 	return (message);
 }
 
+// char	*ft_error_message_operator(char *str, int skip)
+// {
+// 	int	i;
+// 	int	j;
+// 	char c;
+// 	char tmp_bis[4];
+// 	char *tmp;
+// 	char *message;
+
+// 	i = 0;
+// 	j = 0;
+// 	c = str[0];
+// 	if (str[0])
+// 	if (!tmp_bis[0])
+// 	{
+// 		message = ft_strdup("minishell: syntax error near unexpected token `newline'\n"); //MALLOC
+// 		if (!message)
+// 			return (NULL);
+// 	}
+// 	else
+// 	{
+// 		message = ft_strjoin("minishell: syntax error near unexpected token `", tmp_bis); //MALLOC
+// 		if (!message)
+// 			return (NULL);
+// 		tmp = message;
+// 		message = ft_strjoin(message, "'\n");
+// 		free(tmp);
+// 		if (!message)
+// 			return (NULL);
+// 	}
+// 	return (message);
+// }
+
 int	ft_same_char(char *str)
 {
 	char 	c;
@@ -82,15 +115,21 @@ int	ft_parse_ter(t_msh *msh)
 
 	i = -1;
 	j = 0;
-	// if (msh->av[0].token == CHEVRON && msh->ac == 1)
-	// 	return (ft_error_syntax(ft_error_message_chevron(msh->av[0].data, 1), 2, 1)); // POUR GARANCE : ajouter secu dans ft_error syntax si malloc failed dans message chevron + dans fonction appelent parse_ter
-	// if (msh->av[0].token == OPERATOR || msh->av[])
+	if (msh->av[0].token == CHEVRON && msh->ac == 1)
+		return (ft_error_syntax(ft_error_message_chevron(msh->av[0].data, 1), 2, 1)); // POUR GARANCE : ajouter secu dans ft_error syntax si malloc failed dans message chevron + dans fonction appelent parse_ter
+	if (msh->av[0].token == OPERATOR || msh->av[msh->ac - 1].token == OPERATOR)
+		return (ft_error_syntax("COMMENCE OU FINIT PAR UN OPERATOR\n", 2, 0));
 	while (msh->av[++i].data)
 	{
+		/*CHEVRON*/
 		if (msh->av[i].token == CHEVRON && (ft_strlen(msh->av[i].data) > 2 || ft_same_char(msh->av[i].data) == 1))
 			return (ft_error_syntax(ft_error_message_chevron(msh->av[i].data, 1), 2, 1));
 		if (msh->av[i + 1].data && msh->av[i].token == CHEVRON && msh->av[i + 1].token == CHEVRON)
 			return (ft_error_syntax(ft_error_message_chevron(msh->av[i + 1].data, 0), 2, 1));
+		
+		/*OPERATOR*/
+		if (msh->av[i].token == OPERATOR && (ft_strlen(msh->av[i].data) > 2 || ft_same_char(msh->av[i].data) == 1))
+			return (ft_error_syntax("OPERATOR TOO LONG\n", 2, 0));
 		if (msh->av[i + 1].data && msh->av[i].token == OPERATOR && msh->av[i + 1].token == OPERATOR)
 			return (ft_error_syntax("TO_MUCH OPERATOR\n", 2, 0));
 	}
