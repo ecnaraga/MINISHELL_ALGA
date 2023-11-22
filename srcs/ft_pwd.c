d3dde93c3872c357b63c53c7dcc919e9cc098567
Alix THIEBAUT <athiebau@made-f0Br5s4.clusters.42paris.fr>
2023-11-22 16:42:07 +0100

    alix : cd + pwd

--- srcs/ft_pwd.c
+++ srcs/ft_pwd.c
@@ -6,17 +6,32 @@
 /*   By: athiebau <athiebau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 14:42:04 by athiebau          #+#    #+#             */
-/*   Updated: 2023/11/20 14:52:39 by athiebau         ###   ########.fr       */
+/*   Updated: 2023/11/22 16:15:26 by athiebau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	builtin_pwd(void)
+void	builtin_pwd(char **env)
 {
-	char	*str;
+	int	i;
 
-	str = getcwd(NULL, 0);
-	printf("%s\n", str);
-	free(str);
+	i = -1;
+	while (env[++i])
+	{
+		if (!ft_strncmp(env[i], "PWD=", 4))
+		{
+			printf("%s\n", env[i] + 4);
+			break ;
+		}
+	}
 }
+/*cc srcs/ft_pwd.c libft/src/string/ft_strncmp.c*/
+
+// int main(int ac, char **av, char **env)
+// {
+// 	(void)ac;
+// 	(void)av;
+// 	builtin_pwd(env);
+// 	return 0;
+// }
