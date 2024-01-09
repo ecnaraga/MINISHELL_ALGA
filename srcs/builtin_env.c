6350d15289e229f808b0faee43a19132ddf191fa
gaga <garance.lambey@gmail.com>
2024-01-09 11:57:10 +0100

    merge

--- srcs/builtin_env.c
+++ srcs/builtin_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_env.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: athiebau <athiebau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: galambey <galambey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 17:57:04 by athiebau          #+#    #+#             */
-/*   Updated: 2024/01/04 11:55:17 by athiebau         ###   ########.fr       */
+/*   Updated: 2024/01/09 11:56:37 by galambey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	builtin_env(t_msh *minish)
 {
 	t_env	*new;
 
-	new = *(minish->env);
+	new = *(minish->env);	
 	while (new)
 	{
 		if (new->content[0])
